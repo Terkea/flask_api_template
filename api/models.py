3ba41e00a9091297b81941b7c823c66f14d78c16
Marian Terchila <terkeabt@gmail.com>
2019-11-07 23:05:39 +0000

    Update models.py

--- api/models.py
+++ api/models.py
@@ -17,6 +17,6 @@ class User(db.Model):
     __tablename__ = 'User'
     id = db.Column(db.Integer, primary_key=True)
     public_id = db.Column(db.String(50), unique=True)
-    name = db.Column(db.String(50), unique=True)
+    email = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(255))
     admin = db.Column(db.Boolean)
